a280cccc1a59df8ae075952058923228b8592952
PRAVEEN R L <151341322+Praveen-RL@users.noreply.github.com>
2025-06-30 21:20:29 +0530

    Time: 1 ms (98.75%), Space: 43 MB (57.82%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count=0;
         for(int num:nums){
-            if(((int)Math.log10(num)+1) %2==0){
+            if(((int)Math.log10(num)) %2!=0){
                 count++;
             }
         }
