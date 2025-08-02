a981b6cc2492d88bd77ded4c7a779fcf98e142b2
PRAVEEN R L <151341322+Praveen-RL@users.noreply.github.com>
2025-08-02 23:26:38 +0530

    Time: 6 ms (42.78%), Space: 43.1 MB (75.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,14 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char ch1[]=s.toCharArray();
-        char ch2[]=t.toCharArray();
-        HashMap<Character,Integer> map1=new HashMap<>();
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<ch1.length;i++){
-            map1.put(ch1[i],map1.getOrDefault(ch1[i],0)+1);
+        int arr[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
         }
-        for(int i=0;i<ch2.length;i++){
-            map2.put(ch2[i],map2.getOrDefault(ch2[i],0)+1);
+        for(int count:arr){
+            if(count!=0) return false;
         }
-        return map1.equals(map2);
+        return true;
     }
 }
