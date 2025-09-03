78314d3cc02f1d3736ec079597c7ae5bb948ac02
PRAVEEN R L <151341322+Praveen-RL@users.noreply.github.com>
2025-09-03 10:50:23 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,24 @@
+// Last updated: 9/3/2025, 10:50:17 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        
+        while(start<=end){
+             int mid =start+(end -start)/2;
+            if(nums[mid]==target){
+                return mid;
+                }
+             if(nums[mid]<target){
+               start=mid+1;
+
+                }
+                else{
+                
+                  end=mid-1;}
+
+        }
+      return -1;
+        
+    }
+}
